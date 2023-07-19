a8dd028042fa5f6027cd94fc173d089d003a981a
loganathanravi <loguaustin7788@gmail.com>
2023-07-19 15:26:06 +0530

    changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,10 @@ function Contact() {
   return (
     <div>
        <ul>E-mail: loganathanr7010@gmail.com</ul>
-       <ul>Mobile no: 6385274176</ul>
+       <ul>Mobile no: 6385274176vtuvtf8vtr8vtrv</ul>
     </div>
   )
 }
 
 export default Contact
+
